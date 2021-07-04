0fd0fc63192e038d94329b56bf4079e5d2f8a765
omkar8501 <omkar6142@gmail.com>
2021-07-04 13:13:09 +0530

    libraries added

--- model.py
+++ model.py
@@ -5,3 +5,4 @@ dataset.change_index(netflix)
 netflix_train, netflix_test = dataset.create_test_train_dataset(netflix)
 x_train, y_train = dataset.create_xy_dataset(netflix_train, 1)
 x_test, y_test = dataset.create_xy_dataset(netflix_test, 1)
+
